0c30cbc13e9dd74a54069821de39ad24a1ac17fb
orbitcodeboss <orsonrock0@gmail.com>
2025-10-14 18:49:49 -0700

    first time figuring out JS

---
+++ exercises/ex04.js
@@ -0,0 +1,28 @@
+let totalNumber = 0;
+
+// add a button titled "click me"
+
+$("#greedy-button").click (function(){
+
+totalNumber = totalNumber + 2 ;
+
+let sentence = "You really messed up ";
+let sentencetwo = " farting times"
+
+let totalNumbers= sentence+ totalNumber + sentencetwo;
+
+$("#greedy-button").html(totalNumbers);
+
+
+
+});
+
+// show many times it's been clicked
+// add one to the total number
+// show the total number
+// on our button 
+
+// show "clicked total number of times"
+
+// add a top limit
+

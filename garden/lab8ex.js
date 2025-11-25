176c362f5b4139881335ec8c2e8ab19891aef01d
orbitcodeboss <orsonrock0@gmail.com>
2025-11-24 23:46:55 -0800

    refactoring code

--- garden/lab8ex.js
+++ garden/lab8ex.js
@@ -1,58 +1,49 @@
+let follow = {
+    glasses: false,
+    glasses2: false
+};
 
-let follow =false
-
-$(document).on("dblclick", function() {
-    follow=true;
-    $(document).on("dblclick", function() {
-    follow=false;
-    $(document).on("dblclick", function() {
-    follow=true; });
-    
+$("#glasses").on("dblclick", function () {
+    follow.glasses = !follow.glasses;
+    follow.glasses2 = false; 
 });
-    
-}),
 
+$("#glasses2").on("dblclick", function () {
+    follow.glasses2 = !follow.glasses2;
+    follow.glasses = false; 
+});
 
-
-$(document).mousemove(function(event) {
-  if (follow) {
-    $("#glasses").css({
-    left: event.pageX - 60,
-    top: event.pageY - 60,
-    position: "absolute"
-  });
+$(document).on("mousemove", function (event) {
+    if (follow.glasses) {
+        $("#glasses").css({
+            position: "absolute",
+            left: event.pageX - 60,
+            top: event.pageY - 60
+        });
+    }
+    if (follow.glasses2) {
+        $("#glasses2").css({
+            position: "absolute",
+            left: event.pageX - 60,
+            top: event.pageY - 60
+        });
     }
 });
 
-
-$(document).mousemove(function(event) {
-  if (follow) {
-    $("#glasses2").css({
-    left: event.pageX - 60,
-    top: event.pageY - 60,
-    position: "absolute"
-  });
-    }
+$("#glasses, #glasses2").each(function () {
+    const $el = $(this);
+    $el.data("original-left", $el.css("left"));
+    $el.data("original-top",   $el.css("top"));
 });
 
 
-const originalLeft = $("#glasses").css("left");
-const originalTop = $("#glasses").css("top");
-
 $("#reset").on("click", function () {
-  $("#glasses").css({
-    left: originalLeft,
-    top: originalTop
-  });
-});
-
-
-const originalLeft1 = $("#glasses2").css("left");
-const originalTop1 = $("#glasses2").css("top");
-
-$("#reset").on("click", function () {
-  $("#glasses2").css({
-    left: originalLeft1,
-    top: originalTop1
-  });
+    $("#glasses, #glasses2").each(function () {
+        $(this).css({
+            left: $(this).data("original-left"),
+            top: $(this).data("original-top")
+        });
+    });
+    follow.glasses = false;
+    follow.glasses2 = false;
 });
